9c42ae8d85ef438a4b687003b268d0d65b72ea38
Semsi Gungor <semsi_gungor@hotmail.com>
2023-04-05 16:40:26 +0300

    update cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,37 @@
-import React, { useReducer } from 'react';
-import CartContext from './cart-context';
+import React, { useReducer } from "react";
+import CartContext from "./cart-context";
 
 const ACTIONS = {
-  ADD_ITEM: 'add-item',
-  REMOVE_ITEM: 'remove-item',
+  ADD_ITEM: "add-item",
+  REMOVE_ITEM: "remove-item",
 };
 
 const cartReducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.ADD_ITEM:
-      const updatedItems = [...state.items, action.item];
       const updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
+
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+
+      const existingCartItem = state.items[existingCartItemIndex];
+
+      let updatedItems;
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(action.item);
+      }
+
       return {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
